bb15d8a99abcb93092752782c2b9f85f018c8f3c
贺家麟 <jialin.he@aliensidea.com>
2019-01-19 12:10:12 +0800

    调整docker构建脚本

--- bin/env.sh
+++ bin/env.sh
@@ -5,4 +5,4 @@ PROJECT_PATH=$(cd "../"; pwd)   #工程跟路径
 
 SRC_PATH=${PROJECT_PATH}/src/github.com/KylinHe/aliensboot-server
 
-JS_CLIENT_PATH=${ALIENSBOT_HOME}/aliensboot-client-cocos/assets/Script/aliensbot/protocol/protocol.js  #客户端脚本路径
+JS_CLIENT_PATH=${ALIENSBOOT_HOME}/aliensboot-client-cocos/assets/Script/aliensbot/protocol/protocol.js  #客户端脚本路径
