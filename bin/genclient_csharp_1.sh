a274fef1ef41ac57cfd6f788d52d240635939af0
贺家麟 <jialin.he@aliensidea.com>
2019-03-26 11:30:00 +0800

    新增unity支持

--- bin/genclient_csharp_1.sh
+++ bin/genclient_csharp_1.sh
@@ -5,7 +5,7 @@ filelist=`ls ${SRC_PATH}/protocol/*.proto`
 for file in $filelist
 do
     echo $file
-    protoc --csharp_out=${C_SHARP_CLIENT_PATH}/ --proto_path ${SRC_PATH}/protocol/ $(basename $file .proto).proto
+    protogen -i:$file -o:${C_SHARP_CLIENT_PATH}/$(basename $file .proto).cs
 done
 
 
